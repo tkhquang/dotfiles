6f73bc0a4e8fdc929d104c7715ba346fba975192
Quang Trinh <khacquang.trinh@gmail.com>
2021-02-23 23:30:49 +0700

    Backup at Tue Feb 23 11:30:49 PM +07 2021

--- shell/runcom/10_shell_zsh.sh
+++ shell/runcom/10_shell_zsh.sh
@@ -93,4 +93,16 @@ if has fzf; then
   [[ -f /usr/share/fzf/shell/key-bindings.zsh ]] && source /usr/share/fzf/shell/key-bindings.zsh
 fi
 
+# Fix slowness of pastes with zsh-syntax-highlighting.zsh
+# Taken from https://gist.github.com/magicdude4eva/2d4748f8ef3e6bf7b1591964c201c1ab
+pasteinit() {
+  OLD_SELF_INSERT=${${(s.:.)widgets[self-insert]}[2,3]}
+  zle -N self-insert url-quote-magic # I wonder if you'd need `.url-quote-magic`?
+}
+pastefinish() {
+  zle -N self-insert $OLD_SELF_INSERT
+}
+zstyle :bracketed-paste-magic paste-init pasteinit
+zstyle :bracketed-paste-magic paste-finish pastefinish
+
 # }}}
