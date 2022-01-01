fe98e6495140cd41bf423d5a284dd4b7da09d395
Quang Trinh <khacquang.trinh@gmail.com>
2022-01-01 22:10:37 +0700

    Backup at Sat Jan  1 10:10:37 PM +07 2022

--- scripts/12_post_install.sh
+++ scripts/12_post_install.sh
@@ -19,4 +19,15 @@ done
 echo "+ Restoring Tilix config"
 dconf load /com/gexperts/Tilix/ < ~/dotfiles/tilix.dconf
 
+# # Workaround for Steam notification greyed out
+# # https://github.com/ValveSoftware/steam-for-linux/issues/4735
+# # THis will also enforce using intel graphics card
+# cat << EOF | sudo tee /etc/X11/xorg.conf.d/20-intel.conf
+# Section "Device"
+#    Identifier  "Intel Graphics"
+#    Driver      "intel"
+#    Option      "AccelMethod"  "sna"
+# EndSection
+# EOF
+
 echo ">>> Done!"
