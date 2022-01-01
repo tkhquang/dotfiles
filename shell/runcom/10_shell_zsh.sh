eef11d8f96e6039f7540752cc79ebbe10f00e804
Quang Trinh <khacquang.trinh@gmail.com>
2022-01-01 14:14:19 +0700

    Backup at Sat Jan  1 02:14:19 PM +07 2022

--- shell/runcom/10_shell_zsh.sh
+++ shell/runcom/10_shell_zsh.sh
@@ -112,4 +112,12 @@ pastefinish() {
 zstyle :bracketed-paste-magic paste-init pasteinit
 zstyle :bracketed-paste-magic paste-finish pastefinish
 
+# Fixes a corrupt .zsh_history file
+zsh_fix_history() {
+  mv ~/.zsh_history ~/.zsh_history_bad
+  strings -eS ~/.zsh_history_bad > ~/.zsh_history
+  fc -R ~/.zsh_history
+  rm ~/.zsh_history_bad
+}
+
 # }}}
